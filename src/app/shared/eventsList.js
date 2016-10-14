def9e7a164b6903aaf027c085e6ce3f7893651d2
Isidoro Martinez <isidoro.martinez@petfre.gi>
2016-10-14 15:56:12 +0200

    Coverting raceEvents html to js

--- src/app/shared/eventsList.js
+++ src/app/shared/eventsList.js
@@ -13,107 +13,108 @@ eventsListHttps.send();
 
 var i;
 var j = 0;
+
 function eventsList(data) {
-	var divEL, headerEL, h4EL, h4ELText, aEL, iconEL, aLinkPA, aLinkPAText;
-	var sectionEL, headerSEL, h4SEL, h4SELText;
-	var asideEL, pAEL, articleEL, ulAr, liAr, liArText;
-	var divBtnGroupEL, divSidesRG, divSidesLf, divBtn, aBtn, aBtnText, iconBtn, divSides;
-	divELVertParent = document.createElement('div');
-	divELVertParent.setAttribute('id', data.idDivELVertParent.divId);
-	for (i = 0; i < data.eventsList.length; i++) {
-		divEL = document.createElement('div');
-		divEL.setAttribute('id', data.eventsList[i].idDiv);
-		headerEL = document.createElement('header');
-		headerEL.setAttribute('class', data.eventsList[i].headerClass);
-		h4EL = document.createElement('h4');
-		h4ELText = document.createTextNode(data.eventsList[i].h4Text);
-		aEL = document.createElement('A');
-		aEL.setAttribute('href', data.eventsList[i].aLinkV);
-		iconEL = document.createElement('i');
-		iconEL.setAttribute('class', data.eventsList[i].iconClass);
-		aEL.appendChild(iconEL);
-		h4EL.appendChild(h4ELText);
-		h4EL.appendChild(aEL);
-		headerEL.appendChild(h4EL);
+    var divEL, headerEL, h4EL, h4ELText, aEL, iconEL, aLinkPA, aLinkPAText;
+    var sectionEL, headerSEL, h4SEL, h4SELText;
+    var asideEL, pAEL, articleEL, ulAr, liAr, liArText;
+    var divBtnGroupEL, divSidesRG, divSidesLf, divBtn, aBtn, aBtnText, iconBtn, divSides;
+    divELVertParent = document.createElement('div');
+    divELVertParent.setAttribute('id', data.idDivELVertParent.divId);
+    for (i = 0; i < data.eventsList.length; i++) {
+        divEL = document.createElement('div');
+        divEL.setAttribute('id', data.eventsList[i].idDiv);
+        headerEL = document.createElement('header');
+        headerEL.setAttribute('class', data.eventsList[i].headerClass);
+        h4EL = document.createElement('h4');
+        h4ELText = document.createTextNode(data.eventsList[i].h4Text);
+        aEL = document.createElement('A');
+        aEL.setAttribute('href', data.eventsList[i].aLinkV);
+        iconEL = document.createElement('i');
+        iconEL.setAttribute('class', data.eventsList[i].iconClass);
+        aEL.appendChild(iconEL);
+        h4EL.appendChild(h4ELText);
+        h4EL.appendChild(aEL);
+        headerEL.appendChild(h4EL);
 
-		sectionEL = document.createElement('section');
-		sectionEL.setAttribute('class', data.eventsList[i].sectionClass);
-		headerSEL = document.createElement('header');
-		headerSEL.setAttribute('class', data.eventsList[i].headerSClass);
-		h4SEL = document.createElement('h4');
-		h4SELText = document.createTextNode(data.eventsList[i].h4SText);
-		h4SEL.appendChild(h4SELText);
-		headerSEL.appendChild(h4SEL);
-		sectionEL.appendChild(headerSEL);
+        sectionEL = document.createElement('section');
+        sectionEL.setAttribute('class', data.eventsList[i].sectionClass);
+        headerSEL = document.createElement('header');
+        headerSEL.setAttribute('class', data.eventsList[i].headerSClass);
+        h4SEL = document.createElement('h4');
+        h4SELText = document.createTextNode(data.eventsList[i].h4SText);
+        h4SEL.appendChild(h4SELText);
+        headerSEL.appendChild(h4SEL);
+        sectionEL.appendChild(headerSEL);
 
-		asideEL = document.createElement('aside');
-		asideEL.setAttribute('class', data.eventsList[i].asideClass);
-		pAEL = document.createElement('p');
-		pAELText = document.createTextNode(data.eventsList[i].pAText);
-		pAEL.appendChild(pAELText);
-		aLinkPA = document.createElement('a');
-		aLinkPA.setAttribute('href', data.eventsList[i].aLinkPAV);
-		aLinkPA.setAttribute('target', data.eventsList[i].aLinkTargetPAV);
-		aLinkPAText = document.createTextNode(data.eventsList[i].aLinkPAVText);
-		aLinkPA.appendChild(aLinkPAText);
-		pAEL.appendChild(aLinkPA);
-		asideEL.appendChild(pAEL);
-		sectionEL.appendChild(asideEL);
+        asideEL = document.createElement('aside');
+        asideEL.setAttribute('class', data.eventsList[i].asideClass);
+        pAEL = document.createElement('p');
+        pAELText = document.createTextNode(data.eventsList[i].pAText);
+        pAEL.appendChild(pAELText);
+        aLinkPA = document.createElement('a');
+        aLinkPA.setAttribute('href', data.eventsList[i].aLinkPAV);
+        aLinkPA.setAttribute('target', data.eventsList[i].aLinkTargetPAV);
+        aLinkPAText = document.createTextNode(data.eventsList[i].aLinkPAVText);
+        aLinkPA.appendChild(aLinkPAText);
+        pAEL.appendChild(aLinkPA);
+        asideEL.appendChild(pAEL);
+        sectionEL.appendChild(asideEL);
 
-		articleEL = document.createElement('article');
-		ulAr = document.createElement('ul');
-		ulAr.setAttribute('class', data.eventsList[i].ulArClass);
-		for (j = 0; j < data.eventsList[i].liArList.length; j++) {
-			liAr = document.createElement('li');
-			liAr.setAttribute('class', data.eventsList[i].liArList[j].liArListClass);
-			liArText = document.createTextNode(data.eventsList[i].liArList[j].liArListText);
-			spanAr = document.createElement('span');
-			spanArText = document.createTextNode(data.eventsList[i].liArList[j].spanArText);
-			spanAr.appendChild(spanArText);
-			liAr.appendChild(spanAr);
-			ulAr.appendChild(liAr);
-		}
-		articleEL.appendChild(ulAr);
-		sectionEL.appendChild(articleEL);
+        articleEL = document.createElement('article');
+        ulAr = document.createElement('ul');
+        ulAr.setAttribute('class', data.eventsList[i].ulArClass);
+        for (j = 0; j < data.eventsList[i].liArList.length; j++) {
+            liAr = document.createElement('li');
+            liAr.setAttribute('class', data.eventsList[i].liArList[j].liArListClass);
+            liArText = document.createTextNode(data.eventsList[i].liArList[j].liArListText);
+            spanAr = document.createElement('span');
+            spanArText = document.createTextNode(data.eventsList[i].liArList[j].spanArText);
+            spanAr.appendChild(spanArText);
+            liAr.appendChild(spanAr);
+            ulAr.appendChild(liAr);
+        }
+        articleEL.appendChild(ulAr);
+        sectionEL.appendChild(articleEL);
 
-		divBtnGroupEL = document.createElement('div');
-		divBtnGroupEL.setAttribute('class', data.eventsList[i].divBtnGroupELClass);
-		divSidesRG = document.createElement('div');
-		divSidesLf = document.createElement('div');
-		divSidesRG.setAttribute('class', data.eventsList[i].divBtnSidesELClass.divBtnSLfELClass);
-		divSidesLf.setAttribute('class', data.eventsList[i].divBtnSidesELClass.divBtnSRgELClass);
+        divBtnGroupEL = document.createElement('div');
+        divBtnGroupEL.setAttribute('class', data.eventsList[i].divBtnGroupELClass);
+        divSidesRG = document.createElement('div');
+        divSidesLf = document.createElement('div');
+        divSidesRG.setAttribute('class', data.eventsList[i].divBtnSidesELClass.divBtnSLfELClass);
+        divSidesLf.setAttribute('class', data.eventsList[i].divBtnSidesELClass.divBtnSRgELClass);
 
-		for (t = 0; t < data.eventsList[i].divBtnListEL.length; t++) {
-			divBtn = document.createElement('div');
-			divBtn.setAttribute('class', data.eventsList[i].divBtnListEL[t].divBtnClass);
-			aBtn = document.createElement('a');
-			aBtn.setAttribute('href', data.eventsList[i].divBtnListEL[t].aBtnClass);
-			aBtnText = document.createTextNode(data.eventsList[i].divBtnListEL[t].aBtnText);
-			iconBtn = document.createElement('i');
-			iconBtn.setAttribute('class', data.eventsList[i].divBtnListEL[t].iconBtnClass);
-			aBtn.appendChild(aBtnText);
-			aBtn.appendChild(iconBtn);
-			divBtn.appendChild(aBtn);
-			if (data.eventsList[i].divBtnListEL[t / 2]) {
-				divSidesRG.appendChild(divBtn);
-			} else {
-				divSidesLf.appendChild(divBtn);
-			}
-		}
-		
-		divBtnGroupEL.appendChild(divSidesLf);
-		divBtnGroupEL.appendChild(divSidesRG);
-		articleEL.appendChild(divBtnGroupEL);
+        for (t = 0; t < data.eventsList[i].divBtnListEL.length; t++) {
+            divBtn = document.createElement('div');
+            divBtn.setAttribute('class', data.eventsList[i].divBtnListEL[t].divBtnClass);
+            aBtn = document.createElement('a');
+            aBtn.setAttribute('href', data.eventsList[i].divBtnListEL[t].aBtnClass);
+            aBtnText = document.createTextNode(data.eventsList[i].divBtnListEL[t].aBtnText);
+            iconBtn = document.createElement('i');
+            iconBtn.setAttribute('class', data.eventsList[i].divBtnListEL[t].iconBtnClass);
+            aBtn.appendChild(aBtnText);
+            aBtn.appendChild(iconBtn);
+            divBtn.appendChild(aBtn);
+            if (data.eventsList[i].divBtnListEL[t / 2]) {
+                divSidesRG.appendChild(divBtn);
+            } else {
+                divSidesLf.appendChild(divBtn);
+            }
+        }
+
+        divBtnGroupEL.appendChild(divSidesLf);
+        divBtnGroupEL.appendChild(divSidesRG);
+        articleEL.appendChild(divBtnGroupEL);
 
 
-		divEL.appendChild(headerEL);
-		divEL.appendChild(sectionEL);
-		divELVertParent.appendChild(divEL);
-	}
+        divEL.appendChild(headerEL);
+        divEL.appendChild(sectionEL);
+        divELVertParent.appendChild(divEL);
+    }
 
-	document.getElementById('eventsList').appendChild(divELVertParent);
+    document.getElementById('eventsList').appendChild(divELVertParent);
 }
 
 function init() {
-	eventsList();
+    eventsList();
 }
--- src/app/shared/raceEvents.js
+++ src/app/shared/raceEvents.js
@@ -11,38 +11,100 @@ raceEvListHttps.onreadystatechange = function() {
 raceEvListHttps.open("GET", urlbanners, true);
 raceEvListHttps.send();
 
-var i;
+var i, j, k, z;
 
 function raceEvList(data) {
-	var divBgModel, sectionRE, headerRE, h4RE, h4REText, aRE, iconRE, aLinkPA, aLinkPAText;
-	var articleRE;
-	divBgModel = document.createElement('div');
-	divBgModel.setAttribute('id', data.divBgModeId);
-	divBgModel.setAttribute('class', data.divBgModeClass);
-	sectionRE = document.createElement('section');
-	sectionRE.setAttribute('class', data.sectionClass);
-	headerRE = document.createElement('header');
-	headerRE.setAttribute('class', data.headerClass);
-	h4RE = document.createElement('h4');
-	h4REText = document.createTextNode(data.h4Text);
-	iconEL = document.createElement('i');
-	iconEL.setAttribute('class', data.iconClass);
-	h4RE.appendChild(h4REText);
-	h4RE.appendChild(iconEL);
-	headerRE.appendChild(h4RE);
-	sectionRE.appendChild(headerRE);
-	divBgModel.appendChild(sectionRE);
-	
-	for (i = 0; i < data.raceEvList.length; i++) {
-		articleRE = document.createElement('article');
-		
-		
-	}
+    var divBgModel, sectionRE, headerRE, h4RE, h4REText, aRE, iconRE, aLinkPA, aLinkPAText;
+    var articleRE, divNumbColm, spanNumColm, pNumbColm, divImgNumb, imgNumb, spanNumb, divDescription, titleDesc, aDesc, listParent, listChildTitle, listChildDesc, divBtn, aBtn, aBtnText;
+    divBgModel = document.createElement('div');
+    divBgModel.setAttribute('id', data.divBgModeId);
+    divBgModel.setAttribute('class', data.divBgModeClass);
+    sectionRE = document.createElement('section');
+    sectionRE.setAttribute('class', data.sectionClass);
+    headerRE = document.createElement('header');
+    headerRE.setAttribute('class', data.headerClass);
+    h4RE = document.createElement('h4');
+    h4REText = document.createTextNode(data.h4Text);
+    iconEL = document.createElement('i');
+    iconEL.setAttribute('class', data.iconClass);
+    h4RE.appendChild(h4REText);
+    h4RE.appendChild(iconEL);
+    headerRE.appendChild(h4RE);
+    sectionRE.appendChild(headerRE);
+    divBgModel.appendChild(sectionRE);
 
-	document.getElementById('raceList').appendChild(divBgModel);
+    for (i = 0; i < data.raceEvList.length; ++i) {
+        articleRE = document.createElement('article');
+        articleRE.setAttribute('class', data.raceEvList[i].articleREClass);
+        divNumbColm = document.createElement('div');
+        divNumbColm.setAttribute('class', data.raceEvList[i].divNumbColmClass);
+        spanNumColm = document.createElement('span');
+        spanNumColmText = document.createTextNode(data.raceEvList[i].spanNumColmTxt);
+        pNumbColm = document.createElement('p');
+        pNumbColm.setAttribute('class', data.raceEvList[i].pNumbColmClass);
+        pNumbColmText = document.createTextNode(data.raceEvList[i].pNumbColmTxt);
 
+        divImgNumb = document.createElement('div');
+        divImgNumb.setAttribute('class', data.raceEvList[i].divImgNumbClass);
+        imgNumb = document.createElement('img');
+        imgNumb.setAttribute('src', data.raceEvList[i].imgNumbSrc);
+        spanNumb = document.createElement('span');
+        spanNumbText = document.createTextNode(data.raceEvList[i].spanNumbTxt);
+
+        divBtn = document.createElement('div');
+        divBtn.setAttribute('class', data.raceEvList[i].divBtnClass);
+        aBtn = document.createElement('a');
+        aBtn.setAttribute('href', data.raceEvList[i].aBtnLink);
+        aBtnText = document.createTextNode(data.raceEvList[i].aBtnTxt);
+
+        divDescription = document.createElement('div');
+        divDescription.setAttribute('class', data.raceEvList[i].divDescriptionClass);
+        titleDesc = document.createElement('h3');
+        aDesc = document.createElement('a');
+        aDesc.setAttribute('href', data.raceEvList[i].aDescLink);
+        aDescText = document.createTextNode(data.raceEvList[i].aDescTxt);
+        listParent = document.createElement('dl');
+        // for (k = 0; k < data.raceEvList[i].descList[0].descTitle.length; k++) {
+        //     listChildTitle = document.createElement('dt');
+        //     listChildTitleText = document.createTextNode(data.raceEvList[i].descList[0].descTitle[k].listChildTitleTxt);
+        //     listChildTitle.appendChild(listChildTitleText);
+        //     listParent.appendChild(listChildTitle);
+
+        //     listChildDesc = document.createElement('dd');
+        //     listChildDescText = document.createTextNode(data.raceEvList[i].descList[1].descDesc[z].listChildDescTxt);
+        //     listChildDesc.appendChild(listChildDescText);
+        //     listParent.appendChild(listChildDesc);
+        // }   
+
+    }
+
+    spanNumColm.appendChild(spanNumColmText);
+    pNumbColm.appendChild(pNumbColmText);
+    divNumbColm.appendChild(spanNumColm);
+    divNumbColm.appendChild(pNumbColm);
+
+    spanNumb.appendChild(spanNumbText);
+    divImgNumb.appendChild(imgNumb);
+    divImgNumb.appendChild(spanNumb);
+
+    aDesc.appendChild(aDescText);
+    titleDesc.appendChild(aDesc);
+    divDescription.appendChild(titleDesc);
+    divDescription.appendChild(listParent);
+
+    aBtn.appendChild(aBtnText);
+    divBtn.appendChild(aBtn);
+
+    articleRE.appendChild(divNumbColm);
+    articleRE.appendChild(divImgNumb);
+    articleRE.appendChild(divDescription);
+    articleRE.appendChild(divBtn);
+    sectionRE.appendChild(articleRE);
+    divBgModel.appendChild(sectionRE);
+
+    document.getElementById('raceList').appendChild(divBgModel);
 }
 
 function init() {
-	raceEvList();
+    raceEvList();
 }
