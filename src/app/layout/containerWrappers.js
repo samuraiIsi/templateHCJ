642ba5815c1615ba10a6df7e3c9d7f25f3688e16
Isidoro Martinez <isidoro.martinez@petfre.gi>
2017-02-08 14:24:00 +0100

    flexibility containerWrapper,sortTable Fx,flag tableDynamic

--- src/app/layout/containerWrappers.js
+++ src/app/layout/containerWrappers.js
@@ -17,11 +17,11 @@ var containerWps = function() {
         for (var i = 0; i < data.containerWps.length; i++) {
             divParent = document.createElement('div');
             divParent.setAttribute('id', 'container-wp-' + data.containerWps[i].containerId);
-            divParent.setAttribute('class', 'flex-container-lg');
+            divParent.setAttribute('class', data.containerWps[i].containerClass);
             for (var j = 0; j < data.containerWps[i].classAmount.length; j++) {
                 articleComp = document.createElement('article');
                 articleComp.setAttribute('id', 'wrapper' + data.containerWps[i].wrapperId[j]);
-                articleComp.setAttribute('class', 'wrapper-' + data.containerWps[i].classAmount[j]);
+                articleComp.setAttribute('class', data.containerWps[i].classAmount[j]);
                 divParent.appendChild(articleComp);
             }
             document.getElementById('container-wrappers-' + data.containerWps[i].containerId).appendChild(divParent);
--- src/app/shared/tables.js
+++ src/app/shared/tables.js
@@ -1,5 +1,5 @@
-var tables = function(){
-	var tablesHttps = new XMLHttpRequest();
+var tables = function() {
+    var tablesHttps = new XMLHttpRequest();
     var urltables = "../../../data/tables.json";
 
     tablesHttps.onreadystatechange = function() {
@@ -13,43 +13,54 @@ var tables = function(){
     tablesHttps.send();
 
     var tables = function(data) {
-    	var divWrapper, h2Title, tableParent, tableHead, tableBody, titlesColumns, rowTable, rowTitleTable, cellTable, cellTitleTable, textTitleTable, textCell;
-        
-    	for(var n = 0; n < data.rankingTitles.length; n++) {
+        var divWrapper, h2Title, tableParent, tableHead, tableBody, titlesColumns, rowTable, rowTitleTable, cellTable, cellTitleTable, spanTitle, textTitleTable, textCell;
+
+        for (var n = 0; n < data.rankingTitles.length; n++) {
             divWrapper = document.createElement('div');
-            
+
         }
 
-        for(var z = 0; z<data.ranking.length; z++) {
+        for (var z = 0; z < data.ranking.length; z++) {
             divWrapper = document.createElement('div');
             divWrapper.setAttribute('class', 'table-qualification');
             h2Title = document.createElement('h2');
             rankingTextTitle = document.createTextNode(data.rankingTitles[z]);
             h2Title.appendChild(rankingTextTitle);
-    		tableParent = document.createElement('table');
+            tableParent = document.createElement('table');
+            tableParent.setAttribute('id', data.tableId[z]);
             tableHead = document.createElement('thead');
             tableBody = document.createElement('tbody');
-	    	for(var i = 1; i<data.ranking[z].length; i++) {			
+            for (var i = 1; i < data.ranking[z].length; i++) {
                 rowTable = document.createElement('tr');
                 rowTitleTable = document.createElement('tr');
-				for(var j = 0; j<data.ranking[z][i].length; j++) {
-					cellTable = document.createElement('td');
+                for (var j = 0; j < data.ranking[z][i].length; j++) {
+                    cellTable = document.createElement('td');
                     cellTitleTable = document.createElement('th');
-					textCell = document.createTextNode(data.ranking[z][i][j]);
-					cellTable.appendChild(textCell);
+                    if(data.flagTableDynamic[z][j] == "off") {
+                        spanTitle = document.createElement('i');
+                        spanTitle.setAttribute('class', null);
+                        cellTitleTable.setAttribute('onclick', null);
+                    }else{
+                        spanTitle = document.createElement('i');
+                        spanTitle.setAttribute('class', 'arrow-up cursor-pointer');
+                        cellTitleTable.setAttribute('onclick', 'sortTable(this)');
+                    }
+                    textCell = document.createTextNode(data.ranking[z][i][j]);
+                    cellTable.appendChild(textCell);
                     textTitleTable = document.createTextNode(data.ranking[z][0][j]);
                     cellTitleTable.appendChild(textTitleTable);
+                    cellTitleTable.appendChild(spanTitle);
                     rowTitleTable.appendChild(cellTitleTable);
-					rowTable.appendChild(cellTable);
-				}
-				tableBody.appendChild(rowTable);
-			}
+                    rowTable.appendChild(cellTable);
+                }
+                tableBody.appendChild(rowTable);
+            }
             tableHead.appendChild(rowTitleTable);
             tableParent.appendChild(tableHead);
             tableParent.appendChild(tableBody);
             divWrapper.appendChild(h2Title);
             divWrapper.appendChild(tableParent);
-            document.getElementById('wrapper' + [z + 3]).appendChild(divWrapper);
-    	}
+            document.getElementById(data.wrapperId[z]).appendChild(divWrapper);
+        }
     };
 };
