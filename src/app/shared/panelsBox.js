c80e9a4c25473175840d572903874ce83870df99
Isidoro Martinez <isidoro.martinez@petfre.gi>
2017-02-09 14:16:29 +0100

    Finish PanelBox,loop WrapperClass +1,bannerSport styles declare

--- src/app/shared/panelsBox.js
+++ src/app/shared/panelsBox.js
@@ -1,51 +1,60 @@
 var panelsBox =  function(){
 	var panelsHttps = new XMLHttpRequest();
-    var urlPanels = "../../../data/panels.json";
+    var urlPanels = "../../../data/panelsBox.json";
 
     panelsHttps.onreadystatechange = function() {
         if (panelsHttps.readyState == 4 && panelsHttps.status == 200) {
             var info = JSON.parse(panelsHttps.responseText);
-            panel(info);
+            panelBox(info);
         }
     };
 
     panelsHttps.open("GET", urlPanels, true);
     panelsHttps.send();
-	  var panel = function(data){
-	  	var article, divParent, divSubParent, divHeader, divBody, anchor, divCaption,divCaptionContent, iconContent, imgCaption, divFooter;
+	  var panelBox = function(data){
+	  	var article, divParent, divHeader, divHeaderText, divBody, anchor, divCaption,divCaptionContent, iconContent, imgCaption, divFooter;
 	  	for(var i = 0; i < data.panelsBox.length; i++) {
 		  	article = document.createElement('article');
+		  	article.setAttribute('class', data.panelsBox[i].articleClass)
 		  	divParent = document.createElement('div');
 		  	divParent.setAttribute('class', data.panelsBox[i].divParentClass);
-		  	divSubParent = document.createElement('div');
-		  	divSubParent.setAttribute('class', data.panelsBox[i].divSubParentClass);
 		  	divHeader = document.createElement('div');
 		  	divHeader.setAttribute('class', data.panelsBox[i].divHeaderClass);
+		  	divHeaderText = document.createTextNode(data.panelsBox[i].divHeaderText)
 		  	divBody = document.createElement('div');
 		  	divBody.setAttribute('class', data.panelsBox[i].divBodyClass);
 		  	anchor = document.createElement('a');
-		  	anchor.setAttribute('class', data.panelsBox[i].anchorClass);
 		  	anchor.setAttribute('href', data.panelsBox[i].anchorLink);
+		  	anchor.setAttribute('class', data.panelsBox[i].anchorClass);
 		  	divCaption = document.createElement('div');
 		  	divCaption.setAttribute('class', data.panelsBox[i].divCaptionClass);
 		  	divCaptionContent = document.createElement('div');
-		  	divCaptionContent.setAttribute('class',data.panelsBox[i].divCaptionClass);
+		  	divCaptionContent.setAttribute('class',data.panelsBox[i].divCaptionContentClass);
 		  	iconContent = document.createElement('i');
 		  	iconContent.setAttribute('class', data.panelsBox[i].iconContentClass);
 		  	imgCaption = document.createElement('img');
 		  	imgCaption.setAttribute('src', data.panelsBox[i].imgCaptionSrc);
 		  	imgCaption.setAttribute('class', data.panelsBox[i].imgCaptionClass);
 		  	imgCaption.setAttribute('alt', data.panelsBox[i].imgCaptionAlt);
-		  	divCaptionContent.appendChild(imgCaption);
+		  	imgCaption.setAttribute('width', data.panelsBox[i].imgCaptionMaxWidth);
+		  	imgCaption.setAttribute('height', data.panelsBox[i].imgCaptionMaxHeight);
+		  	divFooter = document.createElement('div');
+		  	divFooter.setAttribute('class', data.panelsBox[i].divFooterClass);
+		  	divFooterText = document.createTextNode(data.panelsBox[i].divFooterText);
 		  	divCaptionContent.appendChild(iconContent);
-		  	divCaption.appendChild(divCaptionContent)
+		  	divCaption.appendChild(divCaptionContent);
+		  	divCaption.appendChild(imgCaption);
 		  	anchor.appendChild(divCaption);
+		  	anchor.appendChild(imgCaption);
 		  	divBody.appendChild(anchor);
-		  	divSubParent.appendChild(divHeader);
-		  	divSubParent.appendChild(divBody);
-		  	divParent.appendChild(divSubParent);
-
+		  	divHeader.appendChild(divHeaderText);
+		  	divParent.appendChild(divHeader);
+		  	divParent.appendChild(divBody);
+		  	divFooter.appendChild(divFooterText);
+		  	divParent.appendChild(divFooter);
+		  	article.appendChild(divParent);
+		  	document.getElementById('wrapper9').appendChild(article); 
 	  	}
-	  	document.getElementById('wrapper6').appendChild(divParent); 
+	  	
   	};
 };
