4acd1116325e2f00a0ec6e3a1337655c6a51c906
Isidoro Martinez <isidoro.mguerrero@gmail.com>
2017-08-23 12:40:47 -1200

    Content, Gulp & favicon in Live

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass'),
 	uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  ngAnnotate = require('gulp-ng-annotate'),
+  rename = require('gulp-rename'),
 	modernizr = require('gulp-modernizr');
 	loading = require('gulp-load-plugins')();
 
+var jsFiles = 'src/**/**/**/*.js',
+    jsDest = 'dist/';
+
 gulp.task('scripts', function(){
-	return gulp.src('src/app/**/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('build/js'));  
-});
+    return gulp.src(jsFiles)
+    .pipe(gulp.dest(jsDest)) 
+    .pipe(concat('scripts.js'))
+    .pipe(ngAnnotate({add: true}))
+    .pipe(gulp.dest(jsDest)) 
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest(jsDest));
+    //.on('error', gutil.log);
+});   
+// gulp.task('scripts', function(){
+// 	return gulp.src('src/app/**/*.js')
+// 	.pipe(uglify())
+// 	.pipe(gulp.dest('build/js'));  
+// });
 // src/theme/scss/**/**/*.scss' 
 gulp.task('styles', function(){
 	return sass('src/theme/scss/**/**/**/*.scss', {style: 'compressed'})
@@ -29,7 +46,7 @@ gulp.task('modernizr', function() {
   gulp.src('src/app/**/*.js')
     .pipe(modernizr('modernizr.js', settings))
     .pipe(uglify())
-    .pipe(gulp.dest("build/"))
+    .pipe(gulp.dest("dist/"))
 });
 
 gulp.task('watch', function(){
